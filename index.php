e743b7f14a8595733357442e943d86022b5300fb
Jean Perez A <marckope.com@gmail.com>
2023-01-30 09:28:44 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+    $config_diente_arriba = array(
+        array('id'=>'18','class'=>'tercer_molar ','name'=>'buccal.8.png'),
+        array('id'=>'17','class'=>'segundo_molar ','name'=>'buccal.7.png'),
+        array('id'=>'16','class'=>'primer_molar ','name'=>'buccal.6.png'),
+        array('id'=>'15','class'=>'segundo_premolar ','name'=>'buccal.5.png'),
+        array('id'=>'14','class'=>'primer_premolar ','name'=>'buccal.4.png'),
+        array('id'=>'13','class'=>'canino ','name'=>'buccal.3.png'),
+        array('id'=>'12','class'=>'incicivo_lateral ','name'=>'buccal.2.png'),
+        array('id'=>'11','class'=>'incicivo_central ','name'=>'buccal.1.png'),
+        array('id'=>'21','class'=>'incicivo_central rota-horizontal','name'=>'buccal.1.png'),
+        array('id'=>'22','class'=>'incicivo_lateral rota-horizontal','name'=>'buccal.2.png'),
+        array('id'=>'23','class'=>'canino rota-horizontal','name'=>'buccal.3.png'),
+        array('id'=>'24','class'=>'primer_premolar rota-horizontal','name'=>'buccal.4.png'),
+        array('id'=>'25','class'=>'segundo_premolar rota-horizontal','name'=>'buccal.5.png'),
+        array('id'=>'26','class'=>'primer_molar rota-horizontal','name'=>'buccal.6.png'),
+        array('id'=>'27','class'=>'segundo_molar rota-horizontal','name'=>'buccal.7.png'),
+        array('id'=>'28','class'=>'tercer_molar rota-horizontal','name'=>'buccal.8.png'),
+
+    );
+    $config_diente_c1 = array(
+        array('id'=>'18','class'=>'tercer_molar ','name'=>'buccal.8.base.png'),
+        array('id'=>'17','class'=>'segundo_molar ','name'=>'buccal.7.base.png'),
+        array('id'=>'16','class'=>'primer_molar ','name'=>'buccal.6.base.png'),
+        array('id'=>'15','class'=>'segundo_premolar ','name'=>'buccal.5.base.png'),
+        array('id'=>'14','class'=>'primer_premolar ','name'=>'buccal.4.base.png'),
+        array('id'=>'13','class'=>'canino ','name'=>'buccal.3.base.png'),
+        array('id'=>'12','class'=>'incicivo_lateral ','name'=>'buccal.2.base.png'),
+        array('id'=>'11','class'=>'incicivo_central ','name'=>'buccal.1.base.png'),
+
+    );
+    $config_diente_c2 = array(
+        array('id'=>'21','class'=>'incicivo_central rota-horizontal','name'=>'buccal.1.base.png'),
+        array('id'=>'22','class'=>'incicivo_lateral rota-horizontal','name'=>'buccal.2.base.png'),
+        array('id'=>'23','class'=>'canino rota-horizontal','name'=>'buccal.3.base.png'),
+        array('id'=>'24','class'=>'primer_premolar rota-horizontal','name'=>'buccal.4.base.png'),
+        array('id'=>'25','class'=>'segundo_premolar rota-horizontal','name'=>'buccal.5.base.png'),
+        array('id'=>'26','class'=>'primer_molar rota-horizontal','name'=>'buccal.6.base.png'),
+        array('id'=>'27','class'=>'segundo_molar rota-horizontal','name'=>'buccal.7.base.png'),
+        array('id'=>'28','class'=>'tercer_molar rota-horizontal','name'=>'buccal.8.base.png')
+    );
+    $config_diente_abajo = array(
+        array('id'=>'48','class'=>'tercer_molar ','name'=>'buccal.8b.png'),
+        array('id'=>'47','class'=>'segundo_molar ','name'=>'buccal.7b.png'),
+        array('id'=>'46','class'=>'primer_molar ','name'=>'buccal.6b.png'),
+        array('id'=>'45','class'=>'segundo_premolar ','name'=>'buccal.5b.png'),
+        array('id'=>'44','class'=>'primer_premolar ','name'=>'buccal.4b.png'),
+        array('id'=>'43','class'=>'canino ','name'=>'buccal.3b.png'),
+        array('id'=>'42','class'=>'incicivo_lateral ','name'=>'buccal.2b.png'),
+        array('id'=>'41','class'=>'incicivo_central ','name'=>'buccal.1b.png'),
+        array('id'=>'31','class'=>'incicivo_central rota-horizontal','name'=>'buccal.1b.png'),
+        array('id'=>'32','class'=>'incicivo_lateral rota-horizontal','name'=>'buccal.2b.png'),
+        array('id'=>'33','class'=>'canino rota-horizontal','name'=>'buccal.3b.png'),
+        array('id'=>'34','class'=>'primer_premolar rota-horizontal','name'=>'buccal.4b.png'),
+        array('id'=>'35','class'=>'segundo_premolar rota-horizontal','name'=>'buccal.5b.png'),
+        array('id'=>'36','class'=>'primer_molar rota-horizontal','name'=>'buccal.6b.png'),
+        array('id'=>'37','class'=>'segundo_molar rota-horizontal','name'=>'buccal.7b.png'),
+        array('id'=>'38','class'=>'tercer_molar rota-horizontal','name'=>'buccal.8b.png'),
+
+    );
+    $config_diente_c4 = array(
+        array('id'=>'48','class'=>'tercer_molar ','name'=>'buccal.8b.base.png'),
+        array('id'=>'47','class'=>'segundo_molar ','name'=>'buccal.7b.base.png'),
+        array('id'=>'46','class'=>'primer_molar ','name'=>'buccal.6b.base.png'),
+        array('id'=>'45','class'=>'segundo_premolar ','name'=>'buccal.5b.base.png'),
+        array('id'=>'44','class'=>'primer_premolar ','name'=>'buccal.4b.base.png'),
+        array('id'=>'43','class'=>'canino ','name'=>'buccal.3b.base.png'),
+        array('id'=>'42','class'=>'incicivo_lateral ','name'=>'buccal.2b.base.png'),
+        array('id'=>'41','class'=>'incicivo_central ','name'=>'buccal.1b.base.png')
+    ); 
+    
+    $config_diente_c3 = array(
+        array('id'=>'31','class'=>'incicivo_central rota-horizontal','name'=>'buccal.1b.base.png'),
+        array('id'=>'32','class'=>'incicivo_lateral rota-horizontal','name'=>'buccal.2b.base.png'),
+        array('id'=>'33','class'=>'canino rota-horizontal','name'=>'buccal.3b.base.png'),
+        array('id'=>'34','class'=>'primer_premolar rota-horizontal','name'=>'buccal.4b.base.png'),
+        array('id'=>'35','class'=>'segundo_premolar rota-horizontal','name'=>'buccal.5b.base.png'),
+        array('id'=>'36','class'=>'primer_molar rota-horizontal','name'=>'buccal.6b.base.png'),
+        array('id'=>'37','class'=>'segundo_molar rota-horizontal','name'=>'buccal.7b.base.png'),
+        array('id'=>'38','class'=>'tercer_molar rota-horizontal','name'=>'buccal.8b.base.png')
+    );  
+    $path_diente = 'res/img/piezas_dentales/piezas/bucal/';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odontograma</title>
+    <link rel="stylesheet" href="res/css/estilo.css?v=<?php echo date('YmdHis');?>">
+    <link rel="stylesheet" href="res/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <div class="dentadura">
+                    <div class="arriba">
+                        <?php foreach ($config_diente_arriba as $key => $value) { ?>
+                            <div class="diente <?php echo $config_diente_arriba[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_arriba[$key]['name'];?>" alt=""></div>
+                        <?php } ?>            
+                    </div>
+                    <div class="cuadrantes">
+                        <div class="top">
+                            <div class="cuadrante c1">
+                            <?php foreach ($config_diente_c1 as $key => $value) { ?>
+                                <div class="grupo_cuadrante">
+                                    <div class="diente <?php echo $config_diente_c1[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_c1[$key]['name'];?>" alt=""></div>
+                                    <div class="cudrande cid"><?php echo $config_diente_c1[$key]['id'];?></div>
+                                </div>
+                            <?php } ?>
+                            </div>
+                            <div class="cuadrante c2">
+                            <?php foreach ($config_diente_c2 as $key => $value) { ?>
+                                <div class="grupo_cuadrante">
+                                    <div class="diente <?php echo $config_diente_c2[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_c2[$key]['name'];?>" alt=""></div>
+                                    <div class="cudrande cid"><?php echo $config_diente_c2[$key]['id'];?></div>
+                                </div>
+                            <?php } ?>
+                            </div>
+                        </div>
+                        <div class="botton">
+                            <div class="cuadrante c4">
+                            <?php foreach ($config_diente_c4 as $key => $value) { ?>
+                                <div class="grupo_cuadrante">
+                                    <div class="diente <?php echo $config_diente_c4[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_c4[$key]['name'];?>" alt=""></div>
+                                    <div class="cudrande cid"><?php echo $config_diente_c4[$key]['id'];?></div>
+                                </div>
+                            <?php } ?>
+                            </div>
+                            <div class="cuadrante c3">
+                            <?php foreach ($config_diente_c3 as $key => $value) { ?>
+                                <div class="grupo_cuadrante">
+                                    <div class="diente <?php echo $config_diente_c3[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_c3[$key]['name'];?>" alt=""></div>
+                                    <div class="cudrande cid"><?php echo $config_diente_c3[$key]['id'];?></div>
+                                </div>
+                            <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="abajo">
+                        <?php foreach ($config_diente_abajo as $key => $value) { ?>
+                            <div class="diente <?php echo $config_diente_abajo[$key]['class'];?>"><img src="<?php echo $path_diente.$config_diente_abajo[$key]['name'];?>" alt=""></div>
+                        <?php } ?>            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
